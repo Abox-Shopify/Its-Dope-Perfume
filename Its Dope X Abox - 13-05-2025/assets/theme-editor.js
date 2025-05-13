91f3b75b8cdc6d13809e5e76e810a36813a6f394
Abox-Shopify <hardik@niqox.com>
2025-05-13 16:56:56 +0530

    remove theme

--- Its Dope X Abox - 13-05-2025/assets/theme-editor.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("shopify:section:load", (evt) => {
-  // Load and evaluate section specific scripts immediately.
-  evt.target.querySelectorAll("script[src]").forEach((script) => {
-    const s = document.createElement("script");
-    s.src = script.src;
-    document.body.appendChild(s);
-  });
-});
